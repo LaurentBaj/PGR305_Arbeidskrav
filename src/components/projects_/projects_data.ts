95739bf6af91592532a064633b7918d26e300d59
laurent_ Bajrami <laurent_baj@hotmail.com>
2021-09-19 13:33:30 +0200

    Each project have their own employee

--- src/components/projects_/projects_data.ts
+++ src/components/projects_/projects_data.ts
@@ -1,20 +1,38 @@
-import {IProject} from "../../interfaces/interfaces";
+import {IEmployee, IProject} from "../../interfaces/interfaces";
 import {Status} from "../shared/enums";
 import {getEmployees} from "../employees/employee-data";
 
 
-const projects = Array<IProject>(
-    {name: "Fix webpage", status: Status.PENDING, employees:getEmployees()},
-    {name: "Server update", status: Status.START, employees:getEmployees()},
-    {name: "Support", status: Status.FINISHED, employees:getEmployees()}
+const dummyEmp1 = Array<IEmployee>(
+    {name: "James", job_desc: "Backend"},
+    {name: "Jenna", job_desc: "UI/UX"}
+)
+
+const dummyEmp2 = Array<IEmployee>(
+    {name: "Amanda", job_desc: "Backend"},
+    {name: "Robert", job_desc: "UI/UX"},
+    {name: "Dwight", job_desc: "Assistant to the Regional Manager"},
 )
 
 
-export const getProjects = () => {
+const projects = Array<IProject>(
+    {name: "Fix webpage", status: Status.PENDING, employees:dummyEmp1},
+    {name: "Server update", status: Status.START, employees:dummyEmp2},
+)
+
+
+
+// GETTERS
+export const getProjects = () => { // All projects
     return [...projects]
 }
 
-export const getProject = (proj: IProject) => {
+export const getProject = (proj: IProject) => { // Specific project
     let project = projects.find(name => name.name === proj.name)
     return project
 }
+
+
+// POST
+
+
