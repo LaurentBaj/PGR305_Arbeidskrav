6847d0619dd1518a4b66fbf00ef5dd424c75c43d
laurent_ Bajrami <laurent_baj@hotmail.com>
2021-09-24 16:23:35 +0200

    Can add project with context

---
+++ src/components/projects_/AddProject.tsx
@@ -0,0 +1,17 @@
+import {FC, useContext, useState} from "react";
+import {ProjectContext} from "../../contexts/ProjectContext";
+import {ProjectContextType} from "../../types/ProjectContextType";
+import {Status} from "../shared/enums";
+
+export const AddProject:FC = () => {
+    const [newProject, setNewProject] = useState("")
+    const {addProject} = useContext(ProjectContext) as ProjectContextType
+
+    return (
+        <>
+            <h1>Add Project</h1>
+            <input type="text" value={newProject} onChange={e => {setNewProject(e.target.value)}}/>
+            <button onClick={ () => addProject({name: newProject, status: Status.START, employees: []})}>Submit</button>
+        </>
+    )
+}
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,6 +1,7 @@
 import {FC} from "react";
 import {ProjectList} from "../components/projects_/ProjectList";
 import {ProjectProvider} from "../contexts/ProjectContext";
+import {AddProject} from "../components/projects_/AddProject";
 
 
 export const Projects:FC = () => {
@@ -9,7 +10,7 @@ export const Projects:FC = () => {
             <section>
                 <h1>Projects</h1>
                 <ProjectProvider>
-
+                    <AddProject />
                     <ProjectList />
                 </ProjectProvider>
             </section>
